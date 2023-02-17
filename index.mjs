c329190da44a2e8db1f88330fdb195449fd317dd
Prince Olubari <olubari.prince.g@gmail.com>
2023-02-17 18:14:13 +0000

    the prompt waorked well in the console

--- index.mjs
+++ index.mjs
@@ -1,16 +1,18 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown");
+// import fs from "fs";
+// import path from 'path';
+import inquirer from "inquirer";
+// import generateMarkdown from ("./generateMarkdown");
 
 // array of questions for user
-const questions = [
+// const questions = [""
 
-];
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+
+
+// }
 
 // function to initialize program
 function init() {
@@ -19,3 +21,41 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+let {} = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What's your first name",
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: "What's your first name",
+        },
+        {
+            type: 'input',
+            name: 'setUp',
+            message: "What's your first name",
+        },
+        {
+            type: 'input',
+            name: 'credit',
+            message: "What's your last name",
+           
+        },
+
+
+    ])
+
+    // fs.writeFile(
+    //     "README.md",
+    //     `
+    //   # example 
+    //   ${process.argv[2]}
+    //   # license
+    //   ${process.argv[3]}`,
+    //     (err) => (err ? console.error(err) : console.log("Success!"))
+    //   );
