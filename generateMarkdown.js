36422b4216118f6e20968fd81230d08bf5410404
Prince Olubari <olubari.prince.g@gmail.com>
2023-02-21 21:38:09 +0000

    added the Table of Content and License Badge and Description

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,86 @@
 // function to generate markdown for README
+function generateLicenseBadge(license) {
+  if (license == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license == "PERL") {
+    return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+  } else if (license == "IBM") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0";
+  } else {
+    return "";
+  }
+}
+
+function generateLicenseDescription(license, user) {
+  let currentYear = new Date().getFullYear();
+
+  if (license == "MIT") {
+    return `Copyright ${currentYear} ${user}
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+  } else if (license == "PERL") {
+    return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+  } else if (license == "IBM") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0";
+  } else {
+    return "";
+  }
+}
+
 function generateMarkdown(data) {
+  let badge = generateLicenseBadge(data.license);
+
+  let licenseDescription = generateLicenseDescription(
+    data.license,
+    data.gitHubUsername
+  );
+
   return `
    
- # ${data.projectTitle}
-        
- # ${data.installation}
+  
+
+ # ${data.projectTitle} ${badge}
+
+
+ * [Install](#installation)
+ * [Usage](#usage)
+ * [Credits](#credits)
+ * [License](#license)  
+ 
+ 
+ ## Description
+ ${data.description}
+ 
+ 
+ ## Installation
+ 
+ ${data.installation}
+ 
+ ## Usage
+  ${data.usage}
+  
+  ## License
+  ${data.installation}
+  ${licenseDescription}
+  
+  ## Contributions
+  ${data.contributions}
+  
+  
+  ## Tests
+  ${data.tests}
+  
+  
+  ## Questions
+  [${data.gitHubUsername}]()
+  
+  * Email Address: 
+  ${data.email}
+  
   
   
   `;
--- index.mjs
+++ index.mjs
@@ -5,79 +5,86 @@ import generateMarkdown from "./generateMarkdown.js";
 
 // array of questions for user
 const questions = [
-    {
-        type: 'input',
-        name: 'fileName',
-        message: "What is the name of your file? ",
-    },
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: "What is your project title",
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: "What are the installation and instructions required",
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: "Provide instructions and examples for use.",
-       
-    },
-    {
-        type: 'input',
-        name: 'credits',
-        message: "List your collaborators, if any, with links to their GitHub profiles.",
-       
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: "How will you want people to contribute to future developement",
-       
-    },
-    {
-        type: 'input',
-        name: 'links',
-        message: "Add your links to the deployed app here",
-        
-    },
-    
+  {
+    type: "input",
+    name: "fileName",
+    message: "What is the name of your file? ",
+  },
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is your project title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a description of your project.",
+  },
+  {
+    type: "input",
+    name: "gitHubUsername",
+    message: "What is your gitHub Username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your gitHub email address.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation and instructions required",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "List your collaborators, if any, with links to their GitHub profiles.",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "How will you want people to contribute to future developement",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Add your links to the deployed app here",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license type is required?",
+    choices: ["MIT", "PERL", "IBM"] ,
+  },
+  {
+    type: "input",
+    name: "links",
+    message: "Add your links to the deployed app here",
+  },
+  
+
     
 ];
 
 // function to write README file
 async function writeToFile(fileName, data) {
-    
-  await  fs.writeFile(fileName,generateMarkdown(data))
-    console.log("Success")
+  await fs.writeFile(fileName, generateMarkdown(data));
+  console.log("Success");
 }
 
 // function to initialize program
 async function init() {
-    
-    let data = await inquirer
-        .prompt(questions)
+  let data = await inquirer.prompt(questions);
 
-        writeToFile(data.fileName+".md",data)
-
+  writeToFile(data.fileName + ".md", data);
+  
 }
 
 // function call to initialize program
 init();
-
-
-
-    
-
-    // fs.writeFile(
-    //     "README.md",
-    //     `
-    //   # example 
-    //   ${process.argv[2]}
-    //   # license
-    //   ${process.argv[3]}`,
-    //     (err) => (err ? console.error(err) : console.log("Success!"))
-    //   );
